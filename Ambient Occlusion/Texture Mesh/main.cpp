393563d9acf558b94ec3e238dea965972c0a2b48
manuelecapece <74317241+manuelecapece@users.noreply.github.com>
2024-02-23 16:58:25 +0100

    Aggiunto render multiple object

--- Ambient Occlusion/Texture Mesh/main.cpp
+++ Ambient Occlusion/Texture Mesh/main.cpp
@@ -201,6 +201,129 @@ void sphereRegular(camera cam, hittable_list world) {
 	}
 }
 
+void multipleObjects(camera cam, hittable_list world) {
+
+	//Sampler
+	int num_samples = 64;
+	multiJittered* sampler_ptr = new multiJittered(num_samples);
+
+	//Luce ambient occluder
+	ambient_occluder* occluder_ptr = new ambient_occluder(getColor("white"), getColor("white"), getColor("white"), 1.0);
+	occluder_ptr->set_min_amount(0.0);
+	occluder_ptr->set_sampler(sampler_ptr);
+
+	//Cube base
+	mesh* cube = new mesh("../models/cube.obj", "../models/");
+	texture* colore_cubo = new image_texture("../models/grigio.jpg");
+	material* m_cube = new material(getColor("white"), getColor("white"), getColor("white"), 0.8f, 0.0f);
+	auto instance_ptr_cubo = make_shared<instance>(cube, m_cube);
+	instance_ptr_cubo->scale(40.0, 1.0, 40.0);
+	instance_ptr_cubo->translate(0.0f, -0.5f, 0.0f);
+	m_cube->texture = colore_cubo;
+	world.add(instance_ptr_cubo);
+
+	//Cube lato 1
+	texture* colore_cubo_lato1 = new image_texture("../models/verde.jpg");
+	material* m_cube_lato1 = new material(getColor("green"), getColor("green"), getColor("white"), 0.8f, 0.0f);
+	auto instance_ptr_cubo_lato1 = make_shared<instance>(cube, m_cube_lato1);
+	instance_ptr_cubo_lato1->scale(1.0, 40.0, 40.0);
+	instance_ptr_cubo_lato1->rotate_y(-10.0f);
+	instance_ptr_cubo_lato1->translate(-2.5f, 20.0f, 5.0f);
+	m_cube_lato1->texture = colore_cubo_lato1;
+	world.add(instance_ptr_cubo_lato1);
+
+	//Cube lato 2
+	texture* colore_cubo_lato2 = new image_texture("../models/giallo.jpg");
+	material* m_cube_lato2 = new material(getColor("yellow"), getColor("yellow"), getColor("white"), 0.8f, 0.0f);
+	auto instance_ptr_cubo_lato2 = make_shared<instance>(cube, m_cube_lato2);
+	instance_ptr_cubo_lato2->scale(0.5, 40.0, 40.0);
+	instance_ptr_cubo_lato2->rotate_y(90.0f);
+	instance_ptr_cubo_lato2->translate(6.0f, 20.0f, -1.5f);
+	m_cube_lato2->texture = colore_cubo_lato2;
+	world.add(instance_ptr_cubo_lato2);
+
+	////Gatto 
+	//mesh* cat = new mesh("../models/cat.obj", "../models/");
+	//texture* colore_cat = new image_texture("../models/bianco.jpg");
+	//material* m_cat = new material(getColor("blue"), getColor("blue"), getColor("white"), 0.8f, 0.0f);
+	//auto instance_ptr_cat = make_shared<instance>(cat, m_cat);
+	//instance_ptr_cat->scale(0.05f, 0.05f, 0.05f);
+	//instance_ptr_cat->rotate_y(90.0f);
+	//instance_ptr_cat->rotate_z(90.0f);
+	//instance_ptr_cat->translate(0.0f, 0.0f, 0.0f);
+	//m_cat->texture = colore_cat;
+	//world.add(instance_ptr_cat);
+
+	////Coniglio 
+	//mesh* bunny = new mesh("../models/bunny.obj", "../models/");
+	//texture* colore_bunny = new image_texture("../models/bianco.jpg");
+	//material* m_bunny = new material(getColor("blue"), getColor("blue"), getColor("white"), 0.8f, 0.0f);
+	//auto instance_ptr_bunny = make_shared<instance>(bunny, m_bunny);
+	//m_bunny->texture = colore_bunny;
+	//world.add(instance_ptr_bunny);
+
+	//Luce point_light
+	point3 light_position(0.0f, 10.0f, 0.0f);
+	point_light* worldlight = new point_light(light_position, getColor("lightgray"), getColor("lightgray"), getColor("lightgray"));
+	world.add_lights(worldlight);
+
+	cam.lookfrom = point3(2, 2, 8);
+	cam.lookat = point3(0, 1, 0);
+	cam.samples_per_pixel = 1;
+	cam.initialize();
+
+	cam.parallel_render_ambient_occlusion(world, *occluder_ptr);
+	SDL_RenderPresent(renderer);
+	string path = "../../screenshot/multipleObjects/num_samples-" + to_string(num_samples) + ".bmp";
+	saveScreenshotBMP(path);
+}
+
+void multipleLights(camera cam, hittable_list world) {
+
+	//Sampler
+	int num_samples = 16;
+	multiJittered* sampler_ptr = new multiJittered(num_samples);
+	//regular* sampler_ptr = new regular(num_samples);
+
+	//Luce ambient occluder
+	ambient_occluder* occluder_ptr = new ambient_occluder(getColor("white"), getColor("white"), getColor("white"), 1.0);
+	occluder_ptr->set_min_amount(0.0);
+	occluder_ptr->set_sampler(sampler_ptr);
+	//world.set_ambient_light(occluder_ptr);
+
+	//Cube base
+	mesh* cube = new mesh("../models/cube.obj", "../models/");
+	texture* colore_cubo = new image_texture("../models/grigio.jpg");
+	material* m_cube = new material(getColor("white"), getColor("white"), getColor("white"), 0.8f, 0.0f);
+	auto instance_ptr_cubo = make_shared<instance>(cube, m_cube);
+	instance_ptr_cubo->scale(40.0, 1.0, 40.0);
+	instance_ptr_cubo->translate(0.0f, -0.5f, 0.0f);
+	m_cube->texture = colore_cubo;
+	world.add(instance_ptr_cubo);
+
+	//Sfera principale obj
+	mesh* sfera_obj = new mesh("../models/sfera.obj", "../models/");
+	texture* colore_sfera_obj = new image_texture("../models/giallo.jpg");
+	material* m_sfera_obj = new material(getColor("yellow"), getColor("yellow"), getColor("white"), 0.8f, 0.0f);
+	auto instance_ptr_sfera_obj = make_shared<instance>(sfera_obj, m_sfera_obj);
+	instance_ptr_sfera_obj->scale(0.3, 0.3, 0.3);
+	m_sfera_obj->texture = colore_sfera_obj;
+	world.add(instance_ptr_sfera_obj);
+
+	//Luce point_light
+	point3 light_position(0.0f, 6.0f, 0.0f);
+	point_light* worldlight = new point_light(light_position, getColor("lightgray"), getColor("lightgray"), getColor("lightgray"));
+	world.add_lights(worldlight);
+
+	cam.lookfrom = point3(0, 5, 10);
+	cam.lookat = point3(0, 0.5, 0);
+	cam.samples_per_pixel = 16;
+	cam.initialize();
+
+	cam.parallel_render(world, *worldlight);
+	SDL_RenderPresent(renderer);
+}
+
 int main(int argc, char* argv[])
 {
 	hittable_list world;
@@ -212,8 +335,10 @@ int main(int argc, char* argv[])
 	cam.vfov = 20;
 
 	cout << "--- Menu" << endl;
-	cout << "* Press a for random  sampling sphere-plane" << endl;
+	cout << "* Press a for random sampling sphere-plane" << endl;
 	cout << "* Press b for regular sampling sphere-plane" << endl;
+	cout << "* Press c for multiple objects scene" << endl;
+	cout << "* Press d for multiple lights scene" << endl;
 	cout << "* Press s for make a screenshot" << endl;
 
 	cam.initialize();
@@ -250,6 +375,15 @@ int main(int argc, char* argv[])
 			case SDLK_b:
 				sphereRegular(cam, world);
 				break;
+
+			case SDLK_c:
+				multipleObjects(cam, world);
+				break;
+
+			case SDLK_d:
+				multipleLights(cam, world);
+				break;
+
 			}
 
 		}
